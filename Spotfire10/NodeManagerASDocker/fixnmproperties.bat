91bae2089132b8de5d64ac6be88e38e04c432242
pmckinnis <45436976+pmckinnis@users.noreply.github.com>
2019-05-29 15:45:00 -0400

    Updated scripts for 10.3.0 and added config build-arg
    Updated paths and service names for 10.3.0 and added a build-arg to support configuration at the time of building the Docker image

--- Spotfire10/NodeManagerASDocker/fixnmproperties.bat
+++ Spotfire10/NodeManagerASDocker/fixnmproperties.bat
@@ -1,18 +1,12 @@
 REM Make sure to modify the installation directory when installing another version
+REM Update Path to config if needed
 
 PUSHD .
 
-cd \tibco\tsnm\10.0.1\nm\config
+cd \tibco\tsnm\10.3.0\nm\config
 
 REM ONLY perform the action if the nodemanager has not already started
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '\fixnmproperties.ps1'"
-REM "cat nodemanager.test | % {$_ -replace \"nodemanager.host.names=.*$\", \"nodemanager.host.names=$env:computername\"} > nodemanager.out"
-
-REM del nodemanager.properties
-REM ren nodemanager.out nodemanager.properties
-
-REM sc config "WpNmRemote7110" start=auto
-REM net start "WpNmRemote7110"
 
 POPD
--- Spotfire10/NodeManagerWPDocker/fixnmproperties.ps1
+++ Spotfire10/NodeManagerWPDocker/fixnmproperties.ps1
@@ -1,17 +1,12 @@
-## MAKE SURE TO MODIFY THE SERVICE NAME for the appropriate version
-
-#cat nodemanager.properties | % {$_ -replace "nodemanager.host.names=.*$", "nodemanager.host.names=$env:computername"} > nodemanager.out
-##$nmprops = Get-Content .\nodemanager.properties
-##$nmprops.replace("nodemanager.host.names=.*$", "nodemanager.host.names=$env:computername") | out-file .\nodemanager.properties2 -force -encoding ascii
+## TODO: Change service name if needed
 
 ## Check to see if supervisor already changed file or not
-
 $modBySupervisor = Get-Content ".\nodemanager.properties" | %{$_ -match "Supervisor changed"}
 
 if (!($modBySupervisor -contains $true))
 {
 	(Get-Content .\nodemanager.properties)  -replace "nodemanager.host.names=.*$", "nodemanager.host.names=$env:computername" | Set-Content .\nodemanager.properties -force -encoding ascii
 
-	sc.exe config "WpNmRemote1001" start=auto
-	net.exe start "WpNmRemote1001"
+	sc.exe config "WpNmRemote1030" start=auto
+	net.exe start "WpNmRemote1030"
 }
